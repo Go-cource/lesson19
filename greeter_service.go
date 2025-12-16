3899d5ae0ec259a1b511ab5a9fe6d672729779ea
Fisherman August <fshrmn.august@gmail.com>
2025-12-16 21:19:19 +0300

    greeter-0

--- greeter_service.go
+++ greeter_service.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func greeterHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/greeter/", greeterHandler)
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatal("8081 - error")
+	}
+}
