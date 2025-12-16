60da22382b2f2610fa6d21533a21dcc8007a88d4
Fisherman August <fshrmn.august@gmail.com>
2025-12-16 21:07:53 +0300

    us1

--- user_service.go
+++ user_service.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+func userHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	
-	err := http.ListenAndServe("8000", nil)
-	if err != nil {
-		log.Fatal("8000 - error")
+
+	http.HandleFunc("/user/", userHandler)
+
+	log.Println("User service started...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("8000 - err")
 	}
+
 }
